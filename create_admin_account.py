e96b6cb1ebcc7a4b0bc6bba71f29e53813ba122d
Ted <t.aldentempleman@gmail.com>
2023-12-13 23:30:15 +0000

    Refactored create_admin_account.py
    Cleaner code and now accepts email and password via command line arguments

--- create_admin_account.py
+++ create_admin_account.py
@@ -1,24 +1,29 @@
 from models import insert_user
 from helpers import generate_salt, hash_password
 from pyotp import random_base32
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Create an admin account with a given username and password")
+
+parser.add_argument("email", action="store", help="Admin account email")
+parser.add_argument("password", action="store", help="Admin account password")
 
 
-def create_admin_account():
-    username = "admin"
-    password = "admin"
+def create_admin_account(email, password, name="Admin", phone="1234"):
     salt = generate_salt()
-    hashed_password = hash_password(password, salt)
-    email = "t.aldentempleman@gmail.com"
-    phone = "000"
+    hashed = hash_password(password, salt)
     twofa_secret = random_base32()
-    is_verified = True
-    is_admin = True
-    
     try:
-        insert_user(username, hashed_password, salt, email, phone, twofa_secret, True, True)
+        insert_user(name, hashed, salt, email, phone, twofa_secret, True, True)
     except Exception as e:
         print("Error creating admin account:", e)
 
 
 if __name__ == "__main__":
-    create_admin_account()
+    args = parser.parse_args()    
+    create_admin_account(args.email, args.password)
+    
+    print("Successfully created admin account.")
+    print(f"Email: {args.email}")
+    print(f"Password: {args.password}")
